015c2544a12c903cc4dc400d2393bef386ab22b5
Alex Sazonov <sas145alex@gmail.com>
2017-04-16 15:42:35 +0300

    before action cable F5

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,11 @@
 module ApplicationHelper
+  def hidden_div_if(condition, attributes = {}, &block)
+    if condition
+      attributes["style"] = "display: none"
+    end
+    content_tag("div", attributes, &block)
+  end
+
   def load_time
     l Time.now
   end
