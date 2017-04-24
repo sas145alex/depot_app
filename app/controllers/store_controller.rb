a44b5de753dc4fcf56c9f4622afe9a1b016faec8
Alex Sazonov <sas145alex@gmail.com>
2017-04-24 21:32:09 +0300

    Playtime

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -4,6 +4,7 @@ class StoreController < ApplicationController
 
   before_action :inc_ind_counter, only: [:index]
   before_action :set_cart
+  skip_before_action :authorize
 
   def index
     @products = Product.order(:title)
